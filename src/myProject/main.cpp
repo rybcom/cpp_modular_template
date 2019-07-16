7c729fe2ba8b4d1fba07f332a0c0193f756aa2e9
michal rybarik tn <michal.rybarik@gmail.com>
2019-07-16 12:15:58 +0200

    update key state

--- src/myProject/main.cpp
+++ src/myProject/main.cpp
@@ -1,7 +1,10 @@
 
 #include <iostream>
+
 #include "Logger.h"
 #include "Configuration.h"
+#include "KeyboardEnums.h"
+#include "Keyboard.h"
 
 namespace 
 {
@@ -14,11 +17,36 @@ namespace
 			WelocomePhrase = get_string("intro", "welcome_phrase", WelocomePhrase);
 		}
 	} settings ;
+
+	bool bExit = false;
+
+	void key_event_action(user_input::Key key)
+	{
+		if (key == user_input::Key::Escape)
+		{
+			INFO_LOG(main, "handled request to terminate");
+
+			bExit = true;
+		}
+	};
+
+
 }
 
 int main()
 {
 	DEFINE_LOGGER(main);
 
-	WARN_LOG_AlWAYS(main, "Welcome : {}",settings.getWelocomePhrase()); 
+	WARN_LOG_AlWAYS(main, "welcome : {}", settings.getWelocomePhrase());
+	WARN_LOG_AlWAYS(main, "press ESC to continue ...");
+
+	user_input::registerHandlerFor_KeyPressed(key_event_action);
+
+	while (!bExit)
+	{
+		user_input::updateKeyboardState();
+	}
+
+	WARN_LOG_AlWAYS(main, "bye-bye");
+
 }

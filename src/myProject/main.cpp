c12253ac81243e691be6550bac56a278917cf94b
michal rybarik tn <michal.rybarik@gmail.com>
2019-07-16 13:00:22 +0200

    show ranges

--- src/myProject/main.cpp
+++ src/myProject/main.cpp
@@ -5,9 +5,15 @@
 #include "Configuration.h"
 #include "KeyboardEnums.h"
 #include "Keyboard.h"
+#include "Ranges.h"
+#include <vector>
+#include <string>
 
 namespace 
 {
+	bool bExit = false;
+	std::vector<std::string> words = { "JULY","MOUSE","TABLE","CAP","WATER","JOHN","TORONTO","CAR" };
+
 	class SettingsConfig : public config::Configuration
 	{
 		ConfigProperty_DefaultValue(std::string, WelocomePhrase, "hello");
@@ -18,7 +24,28 @@ namespace
 		}
 	} settings ;
 
-	bool bExit = false;
+
+	auto constains_key_char(user_input::Key key)
+	{
+		return [key](std::string const & word)
+		{
+			return word.find(char(key)) != std::string::npos;
+		};
+	}
+
+	auto print_word(user_input::Key key)
+	{
+		return [key](std::string const & word)
+		{
+			INFO_LOG(main, "word: {} contain key {}", word, char(key));
+		};
+	}
+
+	void check_key_presence(user_input::Key key)
+	{
+		words | views::filter<std::string>(constains_key_char(key))
+		   	  | views::for_each<std::string>(print_word(key));
+	}
 
 	void key_event_action(user_input::Key key)
 	{
@@ -28,8 +55,12 @@ namespace
 
 			bExit = true;
 		}
-	};
+		else
+		{
+			check_key_presence(key);
+		}
 
+	};
 
 }
 
@@ -38,9 +69,9 @@ int main()
 	DEFINE_LOGGER(main);
 
 	WARN_LOG_AlWAYS(main, "welcome : {}", settings.getWelocomePhrase());
-	WARN_LOG_AlWAYS(main, "press ESC to continue ...");
+	WARN_LOG_AlWAYS(main, "press ESC to exit ...");
 
-	user_input::registerHandlerFor_KeyPressed(key_event_action);
+	user_input::registerHandlerFor_KeyUp(key_event_action);
 
 	while (!bExit)
 	{
